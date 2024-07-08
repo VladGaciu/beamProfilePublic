3d9e470257349ceefaffcc6fb5134cd1271e02cd
Vlad Gaciu <vladjgaciu@gmail.com>
2024-07-08 11:45:33 +0300

    modified:   py3lab.edit1.py

--- py3lab.edit1.py
+++ py3lab.edit1.py
@@ -1,10 +1,9 @@
 print("begin loading libraries")
 
-
 import os
 import tensorflow as tf
 
-gpus = tf.config.experimental.list_physical_devices('GPU')
+gpus = tf.config.experimental.list_physical_devices("GPU")
 print(len(gpus), "Physical GPUs, ")
 
 import numpy as np
@@ -25,7 +24,7 @@ from sklearn.metrics import roc_auc_score
 from numpy import argmax, array
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import OneHotEncoder
-from keras.models import Sequential
+from tensorflow.keras.models import Sequential
 
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
@@ -33,13 +32,12 @@ import matplotlib.image as mpimg
 from keras.preprocessing.image import ImageDataGenerator
 from keras import callbacks
 from keras.metrics import top_k_categorical_accuracy
-from keras.layers import Conv2D, MaxPooling2D
-from keras.layers import Activation, Dropout, Flatten, Dense
+from keras.layers import Conv2D, MaxPooling2D, Conv2DTranspose
+from tensorflow.keras.layers import Activation, Dropout, Flatten, Dense
 from keras import backend as K
 from keras.utils import plot_model
-from keras.metrics import top_k_categorical_accuracy
 
-from tensorflow.keras.layers import Conv2DTranspose, Conv2D
+#from tensorflow.keras.layers import Conv2DTranspose, Conv2D
 
 print("loaded all libraries")
 
@@ -48,7 +46,7 @@ if gpus:
 	# Currently memory growth needs to be the same across GPUs
       for gpu in gpus:
          tf.config.experimental.set_memory_growth(gpu, True)
-      #logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+      #logical_gpus = tf.config.experimental.list_logical_devices("GPU")
       print(len(gpus), "Physical GPUs,")
       #, len(logical_gpus), "Logical GPUs")
    except RuntimeError as e:
@@ -57,13 +55,13 @@ if gpus:
 
 print("setup complete")
 
-train_data_dir='/data/vlad.gaciu/beamProfilePublic/train/'
-validate_data_dir='/data/vlad.gaciu/beamProfilePublic/validation/'
-test_data_dir='/data/vlad.gaciu/beamProfilePublic/test/'
+train_data_dir="/data/vlad.gaciu/beamProfilePublic/train/"
+validate_data_dir="/data/vlad.gaciu/beamProfilePublic/validation/"
+test_data_dir="/data/vlad.gaciu/beamProfilePublic/test/"
 
 # Define constants
-img_height = 400
-img_width = 400
+img_height = 250
+img_width = 250
 
 batch_size_train = 200
 batch_size_test = 100
@@ -75,7 +73,7 @@ input_shape = (img_width, img_height, 1)
 print("\nInitializing Training Data Generator: ")
 
 train_datagen = ImageDataGenerator(
-                                rescale=1. / 255,
+				#rescale=1. / 255,
                                 #width_shift_range=2,
                                 #height_shift_range=2,
                                 #rotation_range=45,
@@ -87,13 +85,13 @@ train_generator = train_datagen.flow_from_directory(
     target_size=(img_width, img_height),
     batch_size=batch_size_train,
     color_mode="grayscale",
-    class_mode='categorical')
+    class_mode="categorical")
 print("")
 
 # Validation Datagen
 print("Initializing Validation Data Generator:")
 validation_datagen = ImageDataGenerator(
-                                rescale=1. / 255,
+				#rescale=1. / 255,
                                 #width_shift_range=2,
                                 #height_shift_range=2,
                                 #rotation_range=45,
@@ -105,15 +103,15 @@ valid_generator = validation_datagen.flow_from_directory(
     target_size=(img_width, img_height),
     batch_size=batch_size_test,
     color_mode="grayscale",
-    class_mode='categorical')
+    class_mode="categorical")
 print("")
 
 # Test Datagen
 print("Initializing Test Data Generator:")
 test_datagen = ImageDataGenerator(
-                                rescale=1. / 255,
+				#rescale=1. / 255,
                                 #width_shift_range=2,
-                                #height_shift_range=2, 
+                                #height_shift_range=2,
                                 #rotation_range=45,
                                 #shear_range=1,
                                 zoom_range=1,
@@ -123,35 +121,40 @@ test_generator = test_datagen.flow_from_directory(
     target_size=(img_width, img_height),
     batch_size=batch_size_test,
     color_mode="grayscale",
-    class_mode='categorical')
+    class_mode="categorical")
 print("")
 
 print("found all data")
 print("\nbegin creating model")
 
 #create model
-model = keras.Sequential()
 
+print("\ndefine model\n")
+model = Sequential()
 print("model initiated")
 
 #Encoder
 model.add(Conv2D(27, (11, 11), input_shape=input_shape))
-model.add(Activation('linear'))
+print("first convolution added")
+model.add(Activation("linear"))
+print("first activation added")
 model.add(MaxPooling2D(pool_size=(4, 4)))
-
+print("first maxpooling 2D added")
+print("first Conv2D loaded")
 model.add(Conv2D(9, (11, 11)))
-model.add(Activation('linear'))
+model.add(Activation("linear"))
 model.add(MaxPooling2D(pool_size=(4, 4)))
+print("second Conv2D loaded")
 print("encoder initiated")
 
 #Decoder
 
 model.add(Conv2DTranspose(9, (13, 13)))
-model.add(Activation('linear'))
+model.add(Activation("linear"))
 #model.add(MaxPooling2D(pool_size=(2, 2)))
 
 model.add(Conv2DTranspose(27, (7, 7)))
-model.add(Activation('linear'))
+model.add(Activation("linear"))
 #model.add(MaxPooling2D(pool_size=(2, 2)))
 
 print("decoder initiated")
@@ -159,15 +162,15 @@ print("decoder initiated")
 #Bottleneck
 
 model.add(Conv2D(1, (4, 4)))
-model.add(Activation('linear'))
+model.add(Activation("linear"))
 
 print("bottleneck initiated")
 
 #Output
 model.add(Flatten())
 model.add(Dense(9))
-model.add(Activation('relu'))
-model.add(Dense(3, activation='softmax'))
+model.add(Activation("relu"))
+model.add(Dense(3, activation="softmax"))
 print("model complete")
 
 output = model.summary()
@@ -181,11 +184,11 @@ output = model.summary()
 print("compile model")
 
 model.compile(optimizer = tf.optimizers.SGD(learning_rate=0.00001),
-              loss = 'SparseCategoricalCrossentropy',
-              metrics = ['SparseCategoricalAccuracy'])
+              loss = "SparseCategoricalCrossentropy",
+              metrics = ["SparseCategoricalAccuracy"])
 
-earlystopping = callbacks.EarlyStopping(monitor='val_sparse_categorical_accuracy', 
-                                        mode='max', 
+earlystopping = callbacks.EarlyStopping(monitor="val_sparse_categorical_accuracy", 
+                                        mode="max", 
                                         patience=5, 
                                         restore_best_weights=True)
 #fit model
@@ -204,12 +207,12 @@ model.evaluate(test_generator)
 
 # Test model prediction accuracy against true data
 
-path = '/data/vlad.gaciu/beamProfilPublic/validation/1/'
+path = "/data/vlad.gaciu/beamProfilPublic/validation/1/"
 dirList = os.listdir(path)
 plt.figure(figsize=(12,20))
 count = 0
 imageList = []
-label = 'Low Confidence'
+label = "Low Confidence"
 
 one = 0
 two = 0
@@ -233,7 +236,7 @@ for i in dirList[:60]:
     classes = model.predict(
     images,
     batch_size=1,
-    verbose='off',
+    verbose="off",
     steps=None,
     callbacks=None,
     max_queue_size=10,
@@ -246,21 +249,21 @@ for i in dirList[:60]:
     #print(f"   This image is {100 * (score):.2f}% far-field.")
     if score > 0.66:
         classifier = f"{100 * (score):.2f}%  wedge"
-        label = ['wedge']
+        label = ["wedge"]
         one += 1
    
     score = float(classes[0][1])
     #print(f"   This image is {100 * (score):.2f}% wedge.")
     if score > 0.66:
         classifier = f"{100 * (score):.2f}%  near-field"
-        label = ['near-field']
+        label = ["near-field"]
         two += 1
     
     score = float(classes[0][2])
     #print(f"   This image is {100 * (score):.2f}% near-field.")
     if score > 0.66:
         classifier= f"{100 * (score):.2f}%  far-field"
-        label = ['far-field']
+        label = ["far-field"]
         three += 1
     
     imageList += label
@@ -271,33 +274,33 @@ for i in dirList[:60]:
         
     if count < 60:
         ax=plt.subplot(10,6,count+1)
-        plt.imshow(image, cmap='gray')
+        plt.imshow(image, cmap="gray")
         plt.title(str(i[0:6] + "   " + classifier), fontsize = 7)
         count += 1
-        #plt.axis('off')
+        #plt.axis("off")
 print("Example Predicted Set:\n\n")
 
 nameList = []
 index = 0
 while index < len(dirList[:]):
-    if dirList[index][0:3] == 'B12':
-        nameList += ['near-field']
-    elif dirList[index][0:3] == 'B11':
-        nameList += ['wedge']
-    elif dirList[index][0:4] == 'ATW1':
-        nameList += ['near-field']
-    elif dirList[index][0:5] == 'FE2_C':
-        nameList += ['wedge']
-    elif dirList[index][0:5] == 'FE2_S':
-        nameList += ['near-field']
-    elif dirList[index][0:4] == 'BTW1':
-        nameList += ['near-field']
-    elif dirList[index][0:3] == 'A12':
-        nameList += ['near-field']
-    elif dirList[index][0:4] == 'ATW2':
-        nameList += ['far-field']
+    if dirList[index][0:3] == "B12":
+        nameList += ["near-field"]
+    elif dirList[index][0:3] == "B11":
+        nameList += ["wedge"]
+    elif dirList[index][0:4] == "ATW1":
+        nameList += ["near-field"]
+    elif dirList[index][0:5] == "FE2_C":
+        nameList += ["wedge"]
+    elif dirList[index][0:5] == "FE2_S":
+        nameList += ["near-field"]
+    elif dirList[index][0:4] == "BTW1":
+        nameList += ["near-field"]
+    elif dirList[index][0:3] == "A12":
+        nameList += ["near-field"]
+    elif dirList[index][0:4] == "ATW2":
+        nameList += ["far-field"]
     else:
-        nameList += ['far-field']
+        nameList += ["far-field"]
     index += 1   
 
 # Create confusion matrix to display results T/F
